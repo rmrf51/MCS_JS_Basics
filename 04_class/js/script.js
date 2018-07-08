7e249de8817f5fa9ae1b517d46646f2741b21ee6
rmrf51 <donat0009@yandex.ru>
2018-07-08 19:54:39 +0300

    yoyo

--- 04_class/js/script.js
+++ 04_class/js/script.js
@@ -1,1 +1,13 @@
 console.log('You are at '+window.location);
+
+const image = document.querySelector('img');
+
+image.hasAttribute('alt');
+console.log(image.hasAttribute('alt'));
+
+image.removeAttribute('alt');
+console.log(image.hasAttribute('alt'));
+
+console.log(image.getAttribute('src'));
+
+image.setAttribute('src','img/round2.png');
